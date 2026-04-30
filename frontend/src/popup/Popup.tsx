e1e8b57fe119aa6cf9c9105b8cb28d70402f2d3d
Mustafa Ünal Öztürk <mustafaunalozturk@gmail.com>
2026-04-30 03:19:48 +0300

    hatalar düzeltildi

--- frontend/src/popup/Popup.tsx
+++
@@ -1,9 +0,0 @@
-export default function Popup() {
-  return (
-   <div className="w-100 h-130 
-   bg-gray-950 text-white 
-   flex items-center justify-center">
-  <p className="text-blue-100 text-xl font-semibold">MeetAgent</p>
-</div>
-  )
-}
